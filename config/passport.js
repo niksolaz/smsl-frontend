3b19b96c107890f376990f103109bd751cbb7ad9
niksolaz <solazzo.nicola@gmail.com>
2015-09-11 16:19:28 +0200

    get user facebook or create new user and save

--- config/passport.js
+++ config/passport.js
@@ -21,4 +21,48 @@ module.exports = function(passport){
 			done(err,user);
 		});
 	});
+
+	//code for login  >>> use('local-login', new LocalStrategy) <<<
+	// code for signup >>> use('local-signup', new LocalStrategy) <<<
+
+	//===================
+	//FACEBOOK ==========
+	//===================
+	passport.use(new FacebookStrategy({
+		//pull in our app id and secret from our auth.js file
+		clientID        : configAuth.facebookAuth.clientID,
+        clientSecret    : configAuth.facebookAuth.clientSecret,
+        callbackURL     : configAuth.facebookAuth.callbackURL
+	},
+	//facebook will send back the token and profile
+	function(token,refreshToken,profile,done){
+		//asynchronous
+		process.nextTick(function(){
+			//find user in database based on their facebook id
+			User.findOne({'facebook.id': profile.id},function(err,user){
+				// if there is an error, stop everything and return that
+                // ie an error connecting to the database
+                if(err) return done(err);
+
+                //if user is found, then log them in
+                if(user){
+                	return done(null,user) //user found, return user
+                }else{
+                	//if not exist user in facebbok, create them
+                	var newUser = new User();
+                	//set facebook info in our user model
+                	newUser.facebook.id    = profile.id; // set the users facebook id                   
+                    newUser.facebook.token = token; // we will save the token that facebook provides to the user                    
+                    newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+                    newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+                    //and save user in database
+                    newUser.save(function(err){
+                    	if(err) throw err;
+                    	//if successful, return the new user
+                    	return done(null,newUser);
+                    });
+                }
+			});
+		});
+	}));
 };
