c83dc5ac5eeb12ad13f941568a78fe4cfee8e2bc
niksolaz <solazzo.nicola@gmail.com>
2015-09-11 17:09:50 +0200

    reconfigured server.js

--- server.js
+++ server.js
@@ -1,64 +1,45 @@
-var express = require('express');
-var path = require('path');
-var httpProxy = require('http-proxy');
-var http = require('http');
-var proxy = httpProxy.createProxyServer({
-  changeOrigin: true,
-  ws: true
-}); 
-var app = express();
-var isProduction = process.env.NODE_ENV === 'production';
-var port = isProduction ? process.env.PORT : 3000;
-var publicPath = path.resolve(__dirname, 'public');
+// server.js
 
-app.use(express.static(publicPath));
+// set up ======================================================================
+// get all the tools we need
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
-app.all('/db/*', function (req, res) {
-  proxy.web(req, res, {
-    target: 'https://glowing-carpet-4534.firebaseio.com/'
-  });
-});
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
 
-if (!isProduction) {
+var configDB = require('./config/database.js');
 
-  var bundle = require('./server/bundle.js');
-  bundle();
-  app.all('/build/*', function (req, res) {
-    proxy.web(req, res, {
-        target: 'http://127.0.0.1:3001'
-    });
-  });
-  app.all('/socket.io*', function (req, res) {
-    proxy.web(req, res, {
-      target: 'http://127.0.0.1:3001'
-    });
-  });
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
 
+// require('./config/passport')(passport); // pass passport for configuration
 
-  proxy.on('error', function(e) {
-    // Just catch it
-  });
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
 
-  // We need to use basic HTTP service to proxy
-  // websocket requests from webpack
-  var server = http.createServer(app);
+app.set('view engine', 'ejs'); // set up ejs for templating
 
-  server.on('upgrade', function (req, socket, head) {
-    proxy.ws(req, socket, head);
-  });
+// required for passport
+app.use(session({ secret: 'ihavesecretsecretsecret' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
-  server.listen(port, function () {
-    console.log('Server running on port ' + port);
-  }); 
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-} else {
-
-  // And run the server
-  app.listen(port, function () {
-    console.log('Server running on port ' + port);
-  });
-
-}
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
 
 
 
