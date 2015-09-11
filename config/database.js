ba0237ab23fa6ab4f0688dcdacc64506dfbd4c1e
niksolaz <solazzo.nicola@gmail.com>
2015-09-11 18:29:40 +0200

    template for facebook login

--- config/database.js
+++ config/database.js
@@ -1,9 +1,6 @@
 // config/database.js
-var mongoAddress = process.env.MONGO_ADDRESS; // e.g. 127.0.0.1
-var mongoAddressPort = process.env.MONGO_ADDRESS_PORT || 27017; // e.g. 27017
-var mongoColletion = process.env.MONGO_COLLECTION; // e.g. smsl/mycollection
-var mongoAddressScheme = "mongodb://";
-var mongoURI = mongoAddressScheme + mongoAddress + ":" + mongoAddressPort + "/" + mongoColletion;
+
+var mongoURI = process.env.MODULUS_SMSL_FRONTEND_URI
 
 module.exports = {
 	// looks example like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
