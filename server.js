4100da6cd27106e5aef5032e988814063c21a960
niksolaz <solazzo.nicola@gmail.com>
2015-09-17 22:20:14 +0200

    added resave and saveIninitialized in session

--- server.js
+++ server.js
@@ -30,7 +30,10 @@ app.use(bodyParser.urlencoded({extended: true})); // get information from html f
 app.set('view engine', 'ejs'); // set up ejs for templating
 
 // required for passport
-app.use(session({ secret: process.env.SECRET_MODULUS })); // session secret
+app.use(session({ 
+				secret: process.env.SECRET_MODULUS,
+				resave: true,
+    			saveUninitialized: true })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
