93892351220121ace3c23019a6689beefe3cec27
Nik Solaz <solazzo.nicola@gmail.com>
2015-09-18 18:02:49 +0000

    Changing the environmental variables for auth

--- config/auth.js
+++ config/auth.js
@@ -2,18 +2,18 @@
 
 module.exports = {
 	'facebookAuth':{
-		'clientID'      : process.env.FACEBOOK_MAT_ID, //app ID
-        'clientSecret'  : process.env.FACEBOOK_MAT_SECRET, //app Secret
+		'clientID'      : process.env.FACEBOOK_SMSL_FRONTEND__ID, //app ID
+        'clientSecret'  : process.env.FACEBOOK_SMSL_FRONTEND__SECRET, //app Secret
         'callbackURL'   : process.env.FACEBOOK_MAT_REDIRECTURI //app URI
 	},
 	'twitterAuth':{
-		'consumerKey'       : process.env.MAT_TWITTER_CONSUMER_KEY,
-        'consumerSecret'    : process.env.MAT_TWITTER_CONSUMER_SECRET,
-        'callbackURL'       : process.env.MAT_TWITTER_CALLBACK_URL
+		'consumerKey'       : process.env.TWITTER_SMSL_FRONTEND__CONSUMER_KEY,
+        'consumerSecret'    : process.env.TWITTER_SMSL_FRONTEND__CONSUMER_SECRET,
+        'callbackURL'       : process.env.TWITTER_SMSL_FRONTEND__CALLBACK_URL
 	},
 	'googleAuth':{
-		'clientID'      : process.env.MAT_GOOGLE_CLIENT_ID,
-        'clientSecret'  : process.env.MAT_GOOGLE_CLIENT_SECRET,
-        'callbackURL'   : process.env.MAT_GOOGLE_URL
+		'clientID'      : process.env.GOOGLE_SMSL_FRONTEND__CLIENT_ID,
+        'clientSecret'  : process.env.GOOGLE_SMSL_FRONTEND__CLIENT_SECRET,
+        'callbackURL'   : process.env.GOOGLE_SMSL_FRONTEND__URL
 	}
 };
