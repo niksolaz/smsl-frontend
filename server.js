540d8c5e3743d5f211e7ee10fd59cc330f8dc07b
niksolaz <solazzo.nicola@gmail.com>
2015-09-19 13:17:57 +0200

    retificated app.listen

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
-app.listen(process.env.IP, process.env.PORT);
+app.listen(port); //listen port request in var port 
 console.log('The magic happens on port ' + port);
 
 
